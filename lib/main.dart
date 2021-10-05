aab64232f7ba0650387bafaf19a94d44e4709496
RoarGrønmo <roar.gronmo@gmail.com>
2021-10-05 12:57:11 +0200

    Push push -  non functional so far...

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,269 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:oauth2/oauth2.dart' as oauth2;
 
-class MyApp extends StatelessWidget {
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+final authorizationEndpoint = Uri.parse('https://login.microsoftonline.com/294c7ede-2387-42ab-bbff-e5eb67ca3aee/oauth2/v2.0/authorize');
+final tokenEndpoint = Uri.parse('https://login.microsoftonline.com/294c7ede-2387-42ab-bbff-e5eb67ca3aee/oauth2/v2.0/token');
+
+final identifier = 'bbe45ebc-fb38-48d8-8abd-9c5a9d38a6fd';
+final secret = 'xR-7Q~wBycyWLmcH58q7qfY-5CJ4RSPSxpysg';
+
+final redirectUrl = Uri.parse('');
+final credentialsFile = File('~/.myapp/credentials.json'); //TODO:Check for correct location ?
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget{
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AAD Oauth Flutter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+          primarySwatch: Colors.blue
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'AAD OAuth Home 2'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+Future<oauth2.Client> createClient() async {
+  var exists = await credentialsFile.exists();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  if(exists) {
+    var credentials =
+        oauth2.Credentials.fromJson(await credentialsFile.readAsString());
+    return oauth2.Client(credentials, identifier: identifier, secret: secret);
   }
 
+  var grant = oauth2.AuthorizationCodeGrant(
+    identifier, authorizationEndpoint, tokenEndpoint, secret: secret);
+
+  var authorizationUrl = grant.getAuthorizationUrl(redirectUrl);
+
+  await redirect(authorizationUrl);
+
+  var responseUrl = await listen(redirectUrl);
+
+  return await grant.handleAuthorizationResponse(responseUrl.queryParameters);
+}
+
+Future<void> redirect(Uri url) async {
+
+}
+
+Future<Uri> listen (Uri url) async {
+
+  return Uri();
+}
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  /*
+  static final Config config = Config(
+      tenant: '294c7ede-2387-42ab-bbff-e5eb67ca3aee',
+      clientId: 'bbe45ebc-fb38-48d8-8abd-9c5a9d38a6fd',
+      scope: 'openid profile offline_access',
+      redirectUri: 'https://login.live.com/oauth20_desktop.srf'
+  );
+  final AadOAuth oauth = AadOAuth(config);
+  */
+  String? accessToken;
+
+  @override
+  Widget build(BuildContext context){
+    //oauth.setWebViewScreenSizeFromMedia(MediaQuery.of(context));
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text(
+              'AzureAD OAuth',
+              style: Theme.of(context).textTheme.headline5,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.launch),
+            title: const Text('Login'),
+            onTap: () {
+              login();
+            },
+          ),
+          ListTile(
+              leading: const Icon(Icons.list_alt),
+              title: const Text('Show me'),
+              onTap: () {
+                listData();
+              }
+          ),
+          ListTile(
+            leading: const Icon(Icons.delete),
+            title: const Text('Logout'),
+            onTap: () {
+              logout();
+            },
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  void showError(dynamic ex){
+    showMessage(ex.toString());
+  }
+
+  void showMessage(String text){
+    var alert = AlertDialog(
+      content: Text(text),
+      actions: <Widget>[
+        TextButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Ok')
+        )
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => alert
+    );
+  }
+
+
+  void listData() async {
+    try{
+
+      final mSlamData = await fetchMSlamData();
+
+      showMessage('User = ${mSlamData.employeeResponse.employeeName}');
+
+    }catch(e){
+      showError(e);
+    }
+  }
+
+  void login() async {
+    /*
+    try{
+      await oauth.login();
+      accessToken = await oauth.getAccessToken();
+      showMessage('Logged in successfully, your access token: $accessToken');
+    }catch(e){
+      showError(e);
+    }*/
+
+  }
+
+  void logout() async
+  {
+    /*
+    await oauth.logout();
+    showMessage('Logged out');
+
+     */
+  }
+
+
+  Future<MSlamData> fetchMSlamData() async {
+
+    accessToken = ''; //await oauth.getAccessToken();
+
+    showMessage(accessToken as String);
+
+    final response = await http.get(
+      Uri.parse('https://api.norva24.no:5010/Slam/meg'),
+      headers: {
+        HttpHeaders.authorizationHeader : 'Bearer ${accessToken as String}'
+      },
+    );
+
+    showMessage(response.body.toString());
+
+    final responseJson = jsonDecode(response.body);
+
+    return MSlamData.fromJson(responseJson);
+  }
+
+
 }
+
+class MSlamData {
+
+  MSlamData({
+    required this.success,
+    required this.message,
+    required this.employeeResponse,
+  });
+
+  final bool? success;
+  final String? message;
+  final MSlamEmployeeResponse employeeResponse;
+
+  factory MSlamData.fromJson(Map<String, dynamic> data) {
+    final success = data['success'] as bool?;
+    final message = data['message'] as String?;
+    //final employeeData = data['employee'] as dynamic;
+
+    final employee = MSlamEmployeeResponse.fromJson(data['data'] as dynamic);
+
+    return MSlamData(
+        success: success,
+        message: message,
+        employeeResponse: employee);
+
+  }
+
+}
+
+
+class MSlamEmployeeResponse{
+
+  MSlamEmployeeResponse({
+    required this.employeeId,
+    required this.companyId,
+    required this.emailAddress,
+    required this.employeeName
+  });
+
+  final int? employeeId;
+  final int? companyId;
+  final String? emailAddress;
+  final String? employeeName;
+
+
+
+  factory MSlamEmployeeResponse.fromJson(Map<String, dynamic> data){
+    final employeeId = data['id'] as int?;
+    final companyId = data['firmaId'] as int?;
+    final emailAddress = data['epostaddresse'] as String?;
+    final employeeName = data['namn'] as String?;
+
+    return MSlamEmployeeResponse(
+        employeeId: employeeId,
+        companyId: companyId,
+        emailAddress: emailAddress,
+        employeeName: employeeName
+    );
+  }
+
+}
+
